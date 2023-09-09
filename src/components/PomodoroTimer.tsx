8e5e10713b36a1485218eb001972afb3626d12d9
murilonicemento <murilo_nascimento22@outlook.com>
2023-09-09 11:29:42 -0300

    feat: criação componente button e timer

---
+++ src/components/PomodoroTimer.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { useInterval } from "../hooks/useInterval";
+import { Button } from "./Button";
+import { Timer } from "./Timer";
+
+interface Props {
+  defaultPomodoroTime: number;
+}
+
+export function PomodoroTimer(props: Props): JSX.Element {
+  const [mainTime, setMainTime] = useState(props.defaultPomodoroTime);
+  useInterval(() => {
+    setMainTime(mainTime - 1);
+  }, 1000);
+
+  return (
+    <div className="pomodoro">
+      <h2>You are: working</h2>
+      <Timer mainTime={mainTime} />
+      <Button text="teste" onClick={() => console.log(1)} />
+    </div>
+  );
+}
